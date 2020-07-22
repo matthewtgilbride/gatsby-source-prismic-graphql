dd4606bd9b7f291af233bcc9899a931408a7e635
Matthew T. Gilbride <mgilbride@chariotsolutions.com>
2020-07-22 18:45:22 -0400

    feat(with-preview-component): provide a WithPreview component
    this may be more developer friendly when using the useStaticQuery hook, since that hook provides the resut of the query directly

--- packages/gatsby-source-prismic-graphql/src/components/withPreview.tsx
+++ packages/gatsby-source-prismic-graphql/src/components/withPreview.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { ComponentType, FC, ReactElement, ReactNode } from 'react';
 import { WrapPage } from './WrapPage';
 
-export const withPreview = (render: Function, query: any, fragments: any = []) => {
+export const withPreview = <T extends object = any>(
+  render: FC<T>,
+  query: any,
+  fragments: any = []
+): FC<T> | null => {
   if (typeof window === 'undefined') {
     return render;
   }
@@ -22,6 +26,42 @@ export const withPreview = (render: Function, query: any, fragments: any = []) =
       options={(window as any).prismicGatsbyOptions || {}}
     >
       <RenderComponent />
+    </WrapPage>
+  );
+};
+
+export type WithPreviewProps<T> = {
+  data: T;
+  query: any;
+  fragments: any[];
+  Component: ComponentType<T>;
+};
+
+export const WithPreview = <T extends object = any>({
+  data,
+  query,
+  fragments,
+  Component,
+}: WithPreviewProps<T>): ReactElement | null => {
+  if (typeof window === 'undefined') {
+    return <Component {...{ ...data }} />;
+  }
+
+  if (!Component) {
+    return null;
+  }
+
+  const rootQuery = `${query.source}${fragments
+    .map((fragment: any) => (fragment && fragment.source ? fragment.source : ''))
+    .join(' ')}`;
+
+  return (
+    <WrapPage
+      data={data}
+      pageContext={{ rootQuery }}
+      options={(window as any).prismicGatsbyOptions || {}}
+    >
+      <Component {...{ ...data }} />
     </WrapPage>
   );
 };
